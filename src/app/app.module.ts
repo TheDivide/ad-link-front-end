09f2caa0c048d817c41a3c298d0ab0128932e2ed
Rufus Ngugi Mbugua <mbuguarufus@gmail.com>
2019-01-28 17:57:24 +0300

    Added Milestone UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeadComponent } from './head/head.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeadComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
